697367e1ff4ac1c62a1f7d94cd77dc17acd04a1b
elidistro7 <elidistro@gmail.com>
2025-01-14 01:15:22 +0300

    netlify issue

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -76,7 +76,7 @@ export default function RootLayout({
 
         {/* JavaScript Libraries */}
         <Script
-          id="jquery-scrip"
+          id="jquery-script"
           src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"
           strategy="lazyOnload"
         />
