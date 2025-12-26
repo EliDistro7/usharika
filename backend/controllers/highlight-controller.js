f59280347661bce830c91c8613a588f9b888b634
elidistro7 <elidistro@gmail.com>
2025-12-26 16:07:39 +0300

    new updates

--- backend/controllers/highlight-controller.js
+++ backend/controllers/highlight-controller.js
@@ -259,7 +259,7 @@ const getRecentHighlights = async (req, res) => {
   try {
     // Calculate the date for 3 days ago
     const threeDaysAgo = new Date();
-    threeDaysAgo.setDate(threeDaysAgo.getDate() - 405);
+    threeDaysAgo.setDate(threeDaysAgo.getDate() - 100);
 
     // Query highlights created within the last 3 days
     const recentHighlights = await Highlight.find({
